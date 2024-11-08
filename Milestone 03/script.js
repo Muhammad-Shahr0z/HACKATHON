3daed91fd48aeeaf57869b372a855d7497846325
MuhammadShahroz <shahroxyt@gmail.com>
2024-11-08 13:34:06 +0500

    Resume Upgraded Static To Dynamically

--- Milestone 03/script.js
+++ Milestone 03/script.js
@@ -1,33 +1,39 @@
+"use strict";
 // Resume Variables;
 // COntact information Variable resume
-var firstUserName = document.getElementById("firstUserName");
-var lastUserName = document.getElementById("lastUserName");
-var profession = document.getElementById("profession");
-var userPhone = document.getElementById("userPhone");
-var userEmail = document.getElementById("userEmail");
-var userAddress = document.getElementById("userAddress");
-var userID = document.getElementById("userID");
-// INput fieled Variables 
-var inputfName = document.getElementById('inputfName');
-var inputlName = document.getElementById('inputlName');
-var inputProfession = document.getElementById('inputProfession');
-var inputNumber = document.getElementById('inputNumber');
-var inputEmail = document.getElementById('inputEmail');
-var inputCnic = document.getElementById('inputCnic');
-var inputAddress = document.getElementById('inputAddress');
-var jobField = document.getElementById("jobField");
-var Educationclass = document.getElementsByClassName('Education-class');
+let firstUserName = document.getElementById("firstUserName");
+let lastUserName = document.getElementById("lastUserName");
+let profession = document.getElementById("profession");
+let userPhone = document.getElementById("userPhone");
+let userEmail = document.getElementById("userEmail");
+let userAddress = document.getElementById("userAddress");
+let userID = document.getElementById("userID");
+// INput fieled Variables
+let inputfName = document.getElementById("inputfName");
+let inputlName = document.getElementById("inputlName");
+let inputProfession = document.getElementById("inputProfession");
+let inputNumber = document.getElementById("inputNumber");
+let inputEmail = document.getElementById("inputEmail");
+let inputCnic = document.getElementById("inputCnic");
+let inputAddress = document.getElementById("inputAddress");
+// function to allow only numbers or deshes in CNIC FIELD
+inputCnic.addEventListener("keypress", (event) => {
+    const char = event.key;
+    if (!/[0-9-]/.test(char)) {
+        event.preventDefault();
+    }
+});
 // AddMoreEducations Function
-var AddMoreEducations = function () {
-    var AddMoreEdu = document.getElementsByClassName('Education-Section')[0];
-    var WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape');
-    var inputOne = document.createElement('input');
-    inputOne.classList.add('Education-class');
+const AddMoreEducations = () => {
+    const AddMoreEdu = document.getElementsByClassName("Education-Section")[0];
+    let WrapperDiv = document.createElement("div");
+    WrapperDiv.classList.add("inputWrape");
+    let inputOne = document.createElement("input");
+    inputOne.classList.add("EducationTitle");
     inputOne.setAttribute("placeholder", "Field of study (e.g.,Computer Science)");
     inputOne.setAttribute("type", "text");
-    var inputTwo = document.createElement('input');
-    inputTwo.classList.add('Education-class');
+    let inputTwo = document.createElement("input");
+    inputTwo.classList.add("EducationDetail");
     inputTwo.setAttribute("placeholder", "Degree, institution,and year");
     inputTwo.setAttribute("type", "text");
     WrapperDiv.appendChild(inputOne);
@@ -35,16 +41,16 @@ var AddMoreEducations = function () {
     AddMoreEdu.appendChild(WrapperDiv);
 };
 // AddMoreExperience Function
-var AddMoreExperience = function () {
-    var AddMoreExp = document.getElementsByClassName('Experience-Section')[0];
-    var WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape');
-    var inputOne = document.createElement('input');
-    inputOne.classList.add('Experience-class');
+const AddMoreExperience = () => {
+    const AddMoreExp = document.getElementsByClassName("Experience-Section")[0];
+    let WrapperDiv = document.createElement("div");
+    WrapperDiv.classList.add("inputWrape");
+    let inputOne = document.createElement("input");
+    inputOne.classList.add("Experience-title");
     inputOne.setAttribute("placeholder", "Job Title (e.g., Software Engineer)");
     inputOne.setAttribute("type", "text");
-    var inputTwo = document.createElement('input');
-    inputTwo.classList.add('Experience-class');
+    let inputTwo = document.createElement("input");
+    inputTwo.classList.add("Experience-detail");
     inputTwo.setAttribute("placeholder", "Company, years, key responsibilities");
     inputTwo.setAttribute("type", "text");
     WrapperDiv.appendChild(inputOne);
@@ -52,45 +58,88 @@ var AddMoreExperience = function () {
     AddMoreExp.appendChild(WrapperDiv);
 };
 // AddMoreSkill Function
-var AddMoreSkills = function () {
-    var AddMoreSkill = document.getElementsByClassName('Skills-Section')[0];
-    var WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape');
-    var inputOne = document.createElement('input');
-    inputOne.classList.add('Skills-class');
+const AddMoreSkills = () => {
+    const AddMoreSkill = document.getElementsByClassName("Skills-Section")[0];
+    let inputOne = document.createElement("input");
+    inputOne.classList.add("Skills-class");
     inputOne.setAttribute("placeholder", "Skill/Expertise (e.g.,HTML,CSS)");
     inputOne.setAttribute("type", "text");
-    var inputTwo = document.createElement('input');
-    inputTwo.classList.add('Skills-class');
-    inputTwo.setAttribute("placeholder", "Skill Level (%) 0 to 100 ?");
-    inputTwo.type = "number";
-    inputTwo.min = "1";
-    inputTwo.max = "100";
-    inputTwo.oninput = function () {
-        var inputElement = this;
-        var value = inputElement.value;
-        inputElement.value = value.replace(/^0+/, '');
-        if (parseInt(inputElement.value) > 100) {
-            inputElement.value = "100";
-        }
-    };
-    WrapperDiv.appendChild(inputOne);
-    WrapperDiv.appendChild(inputTwo);
-    AddMoreSkill.appendChild(WrapperDiv);
+    AddMoreSkill.appendChild(inputOne);
 };
-// GenerateResume Function
-var GenerateResume = function (event) {
+// education Ul create
+let newUlDiv = document.createElement("ul"); //yahan mene new ul create kia hai
+newUlDiv.classList.add("education-div");
+// experience Ul create
+let newUlDiv2 = document.createElement("ul"); //yahan mene new ul create kia hai
+newUlDiv.classList.add("experience-div");
+// Main Function Of GenerateResume
+const GenerateResume = (event) => {
     event.preventDefault();
-    // Veriables Injecting 
+    // Veriables Injecting Contact Info ANd UserName ANd Profession
     firstUserName.innerText = inputfName.value;
     lastUserName.innerText = inputlName.value;
     profession.innerText = inputProfession.value;
     userPhone.innerText = inputNumber.value;
     userEmail.innerText = inputEmail.value;
     userID.innerText = inputCnic.value;
+    console.log(inputCnic.value);
     userAddress.innerText = inputAddress.value;
-};
+    const educationsTitle = document.getElementsByClassName("EducationTitle");
+    const educationsDetail = document.getElementsByClassName("EducationDetail");
+    let TitleArray = [...educationsTitle];
+    let DetailArray = [...educationsDetail];
+    const listItems = TitleArray.map((title, index) => {
+        const detail = DetailArray[index] ? DetailArray[index].value : "";
+        return `
+      <li class="title">${title.value}</li>
+      <li class="detail">${detail}</li>
+    `;
+    }).join("");
+    if (newUlDiv) {
+        newUlDiv.innerHTML = listItems;
+    }
+    let educationContainer = document.getElementById("education-container");
+    educationContainer.appendChild(newUlDiv);
+    // experience
+    const jobTitle = document.getElementsByClassName("Experience-title");
+    const jobDetail = document.getElementsByClassName("Experience-detail");
+    let jobTitleArray = [...jobTitle];
+    let jobDetailArray = [...jobDetail];
+    const listItemsjob = jobTitleArray
+        .map((title, index) => {
+        const detail = jobDetailArray[index] ? jobDetailArray[index].value : "";
+        return `
+      <li class="title">${title.value}</li>
+      <li class="detail">${detail}</li>
+    `;
+    })
+        .join("");
+    if (newUlDiv2) {
+        newUlDiv2.innerHTML = listItemsjob;
+    }
+    let experiencecontainer = document.getElementById("experience-container");
+    experiencecontainer.appendChild(newUlDiv2);
+    // SKILLS SECTION
+    const skills = document.getElementsByClassName("Skills-class");
+    let skillsArray = [...skills];
+    const skillsLoop = skillsArray
+        .map((title, index) => {
+        return `
+      <li>${title.value}</li>
+    `;
+    })
+        .join("");
+    let skillsContainer = document.getElementById("skills-container");
+    if (skillsContainer) {
+        skillsContainer.innerHTML = skillsLoop;
+    }
+    //Hide container-form input fields if genrated resume button clicked
+    const FormContainer = document.getElementById('container-form');
+    const ResumeContainer = document.getElementById('container-resume');
+    FormContainer.style.display = "none";
+    ResumeContainer.style.display = "flex";
+}; //generate resume block end
 // Print resume Functions
-var PrintResume = function () {
+const PrintResume = () => {
     window.print();
 };
--- Milestone 03/script.ts
+++ Milestone 03/script.ts
@@ -1,214 +1,241 @@
 // Resume Variables;
 
-
-
 // COntact information Variable resume
+let firstUserName: HTMLElement = document.getElementById(
+  "firstUserName"
+) as HTMLElement;
+let lastUserName: HTMLElement = document.getElementById(
+  "lastUserName"
+) as HTMLElement;
+let profession: HTMLElement = document.getElementById(
+  "profession"
+) as HTMLElement;
+let userPhone: HTMLElement = document.getElementById(
+  "userPhone"
+) as HTMLElement;
+let userEmail: HTMLElement = document.getElementById(
+  "userEmail"
+) as HTMLElement;
+let userAddress: HTMLElement = document.getElementById(
+  "userAddress"
+) as HTMLElement;
+let userID: HTMLElement = document.getElementById("userID") as HTMLElement;
 
-let firstUserName   :HTMLElement = document.getElementById("firstUserName") as HTMLElement;
-let lastUserName    :HTMLElement = document.getElementById("lastUserName")  as HTMLElement;
-let profession      :HTMLElement = document.getElementById("profession")    as HTMLElement;
-let userPhone       :HTMLElement = document.getElementById("userPhone")     as HTMLElement;
-let userEmail       :HTMLElement = document.getElementById("userEmail")     as HTMLElement;
-let userAddress     :HTMLElement = document.getElementById("userAddress")   as HTMLElement;
-let userID          :HTMLElement = document.getElementById("userID")        as HTMLElement;
+// INput fieled Variables
+let inputfName: HTMLInputElement = document.getElementById(
+  "inputfName"
+) as HTMLInputElement;
+let inputlName: HTMLInputElement = document.getElementById(
+  "inputlName"
+) as HTMLInputElement;
+let inputProfession: HTMLInputElement = document.getElementById(
+  "inputProfession"
+) as HTMLInputElement;
+let inputNumber: HTMLInputElement = document.getElementById(
+  "inputNumber"
+) as HTMLInputElement;
+let inputEmail: HTMLInputElement = document.getElementById(
+  "inputEmail"
+) as HTMLInputElement;
+let inputCnic: HTMLInputElement = document.getElementById(
+  "inputCnic"
+) as HTMLInputElement;
+let inputAddress: HTMLInputElement = document.getElementById(
+  "inputAddress"
+) as HTMLInputElement;
 
-
-
-
-
-
-
-// INput fieled Variables 
-
-let inputfName      :HTMLInputElement = document.getElementById('inputfName')  as HTMLInputElement 
-let inputlName      :HTMLInputElement = document.getElementById('inputlName')  as HTMLInputElement 
-let inputProfession :HTMLInputElement = document.getElementById('inputProfession')  as HTMLInputElement
-let inputNumber     :HTMLInputElement = document.getElementById('inputNumber')  as HTMLInputElement
-let inputEmail      :HTMLInputElement = document.getElementById('inputEmail')  as HTMLInputElement
-let inputCnic       :HTMLInputElement = document.getElementById('inputCnic')  as HTMLInputElement
-let inputAddress    :HTMLInputElement = document.getElementById('inputAddress')  as HTMLInputElement
-
-
-
-
-let jobField        :HTMLElement = document.getElementById("jobField")      as HTMLElement;
-let Educationclass = document.getElementsByClassName('Education-class') as HTMLCollectionOf<HTMLInputElement>;
-
-
-
-
-
-
-
-
-
-
+// function to allow only numbers or deshes in CNIC FIELD
+inputCnic.addEventListener("keypress", (event) => {
+  const char = event.key;
+  if (!/[0-9-]/.test(char)) {
+    event.preventDefault();
+  }
+});
 
 // AddMoreEducations Function
-const AddMoreEducations=()=>{
+const AddMoreEducations = () => {
+  const AddMoreEdu = document.getElementsByClassName("Education-Section")[0];
 
-    const AddMoreEdu = document.getElementsByClassName('Education-Section')[0];
+  let WrapperDiv = document.createElement("div");
+  WrapperDiv.classList.add("inputWrape");
 
+  let inputOne = document.createElement("input");
+  inputOne.classList.add("EducationTitle");
+  inputOne.setAttribute(
+    "placeholder",
+    "Field of study (e.g.,Computer Science)"
+  );
+  inputOne.setAttribute("type", "text");
 
-    let WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape')
+  let inputTwo = document.createElement("input");
+  inputTwo.classList.add("EducationDetail");
+  inputTwo.setAttribute("placeholder", "Degree, institution,and year");
+  inputTwo.setAttribute("type", "text");
 
-    let inputOne = document.createElement('input');
-    inputOne.classList.add('Education-class')
-    inputOne.setAttribute("placeholder","Field of study (e.g.,Computer Science)")
-    inputOne.setAttribute("type","text")
-
-    let inputTwo = document.createElement('input');
-    inputTwo.classList.add('Education-class')
-    inputTwo.setAttribute("placeholder","Degree, institution,and year")
-    inputTwo.setAttribute("type","text")
-
-    WrapperDiv.appendChild(inputOne)
-    WrapperDiv.appendChild(inputTwo)
-    AddMoreEdu.appendChild(WrapperDiv)
- 
-    }
-
+  WrapperDiv.appendChild(inputOne);
+  WrapperDiv.appendChild(inputTwo);
+  AddMoreEdu.appendChild(WrapperDiv);
+};
 
 // AddMoreExperience Function
-const AddMoreExperience=()=>{
-    const AddMoreExp = document.getElementsByClassName('Experience-Section')[0];
+const AddMoreExperience = () => {
+  const AddMoreExp = document.getElementsByClassName("Experience-Section")[0];
 
+  let WrapperDiv = document.createElement("div");
+  WrapperDiv.classList.add("inputWrape");
 
-    let WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape')
+  let inputOne = document.createElement("input");
+  inputOne.classList.add("Experience-title");
+  inputOne.setAttribute("placeholder", "Job Title (e.g., Software Engineer)");
+  inputOne.setAttribute("type", "text");
 
-    let inputOne = document.createElement('input');
-    inputOne.classList.add('Experience-class')
-    inputOne.setAttribute("placeholder","Job Title (e.g., Software Engineer)")
-    inputOne.setAttribute("type","text")
+  let inputTwo = document.createElement("input");
+  inputTwo.classList.add("Experience-detail");
+  inputTwo.setAttribute("placeholder", "Company, years, key responsibilities");
+  inputTwo.setAttribute("type", "text");
 
-    let inputTwo = document.createElement('input');
-    inputTwo.classList.add('Experience-class')
-    inputTwo.setAttribute("placeholder","Company, years, key responsibilities")
-    inputTwo.setAttribute("type","text")
+  WrapperDiv.appendChild(inputOne);
+  WrapperDiv.appendChild(inputTwo);
+  AddMoreExp.appendChild(WrapperDiv);
+};
 
-    WrapperDiv.appendChild(inputOne)
-    WrapperDiv.appendChild(inputTwo)
-    AddMoreExp.appendChild(WrapperDiv)
- 
-    }
- 
 // AddMoreSkill Function
-const AddMoreSkills=()=>{
+const AddMoreSkills = () => {
+  const AddMoreSkill = document.getElementsByClassName("Skills-Section")[0];
+
+  let inputOne = document.createElement("input");
+  inputOne.classList.add("Skills-class");
+  inputOne.setAttribute("placeholder", "Skill/Expertise (e.g.,HTML,CSS)");
+  inputOne.setAttribute("type", "text");
+
+  AddMoreSkill.appendChild(inputOne);
+};
 
 
 
-    const AddMoreSkill = document.getElementsByClassName('Skills-Section')[0];
+// education Ul create
+let newUlDiv: HTMLUListElement = document.createElement("ul"); //yahan mene new ul create kia hai
+newUlDiv.classList.add("education-div");
+
+// experience Ul create
+let newUlDiv2: HTMLUListElement = document.createElement("ul"); //yahan mene new ul create kia hai
+newUlDiv.classList.add("experience-div");
 
 
-    let WrapperDiv = document.createElement('div');
-    WrapperDiv.classList.add('inputWrape')
+// Main Function Of GenerateResume
+const GenerateResume = (event: Event) => {
+  event.preventDefault();
 
-    let inputOne = document.createElement('input');
-    inputOne.classList.add('Skills-class')
-    inputOne.setAttribute("placeholder","Skill/Expertise (e.g.,HTML,CSS)")
-    inputOne.setAttribute("type","text")
+  // Veriables Injecting Contact Info ANd UserName ANd Profession
+  firstUserName.innerText = inputfName.value;
+  lastUserName.innerText = inputlName.value;
+  profession.innerText = inputProfession.value;
+  userPhone.innerText = inputNumber.value;
+  userEmail.innerText = inputEmail.value;
+  userID.innerText = inputCnic.value;
 
-    let inputTwo = document.createElement('input');
-    inputTwo.classList.add('Skills-class')
-    inputTwo.setAttribute("placeholder","Skill Level (%) 0 to 100 ?")
-    inputTwo.type ="number"
-    inputTwo.min = "1";
-    inputTwo.max = "100";
+  console.log(inputCnic.value);
 
-    inputTwo.oninput = function () {
-        let inputElement = this as HTMLInputElement;
-        let value = inputElement.value;
-    
-  
-        inputElement.value = value.replace(/^0+/, '');
-    
+  userAddress.innerText = inputAddress.value;
+
+  const educationsTitle = document.getElementsByClassName(
+    "EducationTitle"
+  ) as HTMLCollectionOf<HTMLInputElement>;
+  const educationsDetail = document.getElementsByClassName(
+    "EducationDetail"
+  ) as HTMLCollectionOf<HTMLInputElement>;
+
+  let TitleArray: any = [...educationsTitle];
+  let DetailArray: any = [...educationsDetail];
+
+  const listItems = TitleArray.map((title: any, index: number) => {
+    const detail = DetailArray[index] ? DetailArray[index].value : "";
+
+    return `
+      <li class="title">${title.value}</li>
+      <li class="detail">${detail}</li>
+    `;
+  }).join("");
+
+  if (newUlDiv) {
+    newUlDiv.innerHTML = listItems;
+  }
+
+  let educationContainer: HTMLDivElement = document.getElementById(
+    "education-container"
+  ) as HTMLDivElement;
+  educationContainer.appendChild(newUlDiv);
+
+  // experience
+
+  const jobTitle = document.getElementsByClassName(
+    "Experience-title"
+  ) as HTMLCollectionOf<HTMLInputElement>;
+  const jobDetail = document.getElementsByClassName(
+    "Experience-detail"
+  ) as HTMLCollectionOf<HTMLInputElement>;
+
+  let jobTitleArray: any = [...jobTitle];
+  let jobDetailArray: any = [...jobDetail];
+
+  const listItemsjob = jobTitleArray
+    .map((title: any, index: number) => {
+      const detail = jobDetailArray[index] ? jobDetailArray[index].value : "";
+
+      return `
+      <li class="title">${title.value}</li>
+      <li class="detail">${detail}</li>
+    `;
+    })
+    .join("");
+
+  if (newUlDiv2) {
+    newUlDiv2.innerHTML = listItemsjob;
+  }
+
+  let experiencecontainer: HTMLDivElement = document.getElementById(
+    "experience-container"
+  ) as HTMLDivElement;
+  experiencecontainer.appendChild(newUlDiv2);
+
+  // SKILLS SECTION
+
+  const skills = document.getElementsByClassName(
+    "Skills-class"
+  ) as HTMLCollectionOf<HTMLInputElement>;
+
+  let skillsArray: any = [...skills];
+
+  const skillsLoop = skillsArray
+    .map((title: any, index: number) => {
+      return `
+      <li>${title.value}</li>
+    `;
+    })
+    .join("");
+
+  let skillsContainer = document.getElementById("skills-container");
+
+  if (skillsContainer) {
+    skillsContainer.innerHTML = skillsLoop;
+  }
+
+
+//Hide container-form input fields if genrated resume button clicked
+
+const FormContainer:HTMLDivElement = document.getElementById('container-form')as HTMLDivElement
+const ResumeContainer:HTMLDivElement = document.getElementById('container-resume')as HTMLDivElement
+
+FormContainer.style.display = "none"
+ResumeContainer.style.display="flex"
+
  
-        if (parseInt(inputElement.value) > 100) {
-            inputElement.value = "100";
-        }
-    
-    };
-
-    WrapperDiv.appendChild(inputOne)
-    WrapperDiv.appendChild(inputTwo)
-    AddMoreSkill.appendChild(WrapperDiv)
- 
-    }
-    
 
 
-
-// GenerateResume Function
-
-const GenerateResume=(event:Event) =>{
-event.preventDefault()
+}; //generate resume block end
 
 
-
-// Veriables Injecting 
-firstUserName.innerText = inputfName.value;
-lastUserName.innerText = inputlName.value;
-profession.innerText=inputProfession.value;
-userPhone.innerText=inputNumber.value;
-userEmail.innerText=inputEmail.value;
-userID.innerText=inputCnic.value;
-userAddress.innerText=inputAddress.value;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-       
-
-
-    
-    
-    
-    // Print resume Functions
-    const PrintResume = ()=>{
-     window.print()
-
-    }
-
-
-
-
-
-
-
-
+// Print resume Functions
+const PrintResume = () => {
+  window.print();
+};
